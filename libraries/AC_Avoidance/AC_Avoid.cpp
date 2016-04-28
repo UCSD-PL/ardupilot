1ddde4c61ea9001fd937208e5d579973d1beee14
Daniel Ricketts <daniel.bmore.ricketts@gmail.com>
2016-04-28 06:34:06 -0700

    Small enhancement in constructor and a concave hard coded boundary.

--- libraries/AC_Avoidance/AC_Avoid.cpp
+++ libraries/AC_Avoidance/AC_Avoid.cpp
@@ -1,12 +1,9 @@
 #include "AC_Avoid.h"
 
-//#include <AP_HAL/AP_HAL.h>
-//const AP_HAL::HAL& myhal = AP_HAL::get_HAL();
-
 /// Constructor
 AC_Avoid::AC_Avoid(const AP_InertialNav& inav)
   : _inav(inav),
-    _nvert(4),
+    _nvert(sizeof(_boundary)/sizeof(*_boundary)),
     _inside_position(Vector2f(0,0)),
     _accel_cms(250.0f),
     _kP(1.0f)
@@ -26,14 +23,12 @@ void AC_Avoid::adjust_velocity(Vector2f &desired_vel) {
     float max_speed = get_max_speed((position_xy - intersect).length());
     float desired_speed = desired_vel.length();
     if (desired_speed >= max_speed && desired_speed > 0) {
-      //      myhal.console->printf("Desired : %f, Max: %f\n", desired_speed, max_speed);
       desired_vel *= max_speed/desired_speed;
     }
     _inside_position = position_xy;
   } else {
     // Outside the fence
-    // Head towards last known inside position at some fixed velocity.
-    //    myhal.console->printf("Outside\n");
+    // Head towards last known inside position using sqrt controller.
     Vector2f error = (_inside_position - position_xy);
     float distance = error.length();
     desired_vel = error * get_max_speed(distance) / distance;
@@ -46,10 +41,4 @@ void AC_Avoid::adjust_velocity(Vector2f &desired_vel) {
  */
 float AC_Avoid::get_max_speed(float distance) {
   return AC_AttitudeControl::sqrt_controller(distance,_kP,_accel_cms);
-  // float linear_distance = _accel_cms/(2.0f*_kP*_kP);
-  // if (distance < linear_distance) {
-  //   return distance*_kP;
-  // } else {
-  //   return safe_sqrt((distance - linear_distance)*2.0f*_accel_cms);
-  // }
 }
